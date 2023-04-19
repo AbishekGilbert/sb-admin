af6b0d42fd72852ac70fe998d7f7c26a33904c6b
AbishekGilbert <abishek.gilbert0511@gmail.com>
2023-04-19 09:13:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
+import BaseApp from "./Base/base";
 import './App.css';
+import LogInPage from "./Pages/loginpage";
+import Register from "./Pages/Register";
+import ForgotPassword from "./Pages/forgot-password";
+import Dashboard from "./childrens/Dashboard";
+import Button from "./Components/button";
+import Cards from "./Components/Cards";
+import TablesReact from "./Table/Table";
+import {BrowserRouter, Route, Router,Routes } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage";
+import BlankPage from "./Pages/BlankPage";
+import Colors from "./utility/Colors";
+import Borders from "./utility/Borders";
+import Animation from "./utility/Animation";
+import Others from "./utility/other";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/components/buttons" element={<Button/>}/>
+        <Route path="/components/cards" element={<Cards/>}/>
+        <Route path="/pages/register" element={<Register/>}/>
+        <Route path="/pages/login" element={<LogInPage/>}/>
+        <Route path="/pages/forgot" element={<ForgotPassword/>}/>
+        <Route path="/pages/error" element={<ErrorPage/>}/>
+        <Route path="/pages/blank" element={<BlankPage/>}/>
+        <Route path="/others/tables" element={<TablesReact/>}/>
+        <Route path="/colors" element={<Colors/>}/>
+        <Route path="/borders" element={<Borders/>}/>
+        <Route path="/animation" element={<Animation/>}/>
+        <Route path="/others" element={<Others/>}/>
+          
+       </Routes>
+       </BrowserRouter>
+     </div>
   );
 }
 
